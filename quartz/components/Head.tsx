3d863b91b333232ea6412add3bfdd11769043f4d
saip_the_king <86388371+saiptheking@users.noreply.github.com>
2026-06-10 13:27:32 -0500

    Add Google site verification meta tag

--- quartz/components/Head.tsx
+++ quartz/components/Head.tsx
@@ -61,7 +61,7 @@ export default (() => {
         )}
         <link rel="preconnect" href="https://cdnjs.cloudflare.com" crossOrigin="anonymous" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-
+        <meta name="google-site-verification" content="GgE-C5RVyJS1tXo9LLxKOc_ETIqbGor57eNsy-J31GI" />
         <meta name="og:site_name" content={cfg.pageTitle}></meta>
         <meta property="og:title" content={title} />
         <meta property="og:type" content="website" />
